96dfce5d339270e96ad7429f2dad49f8a819dfd8
AlexG-BGLL-Laptop <support@lotteries.bz>
2024-10-24 16:46:47 -0600

    Basic Angular Skeleton - Update

--- src/app/Page/dashboard-page/dashboard-page.component.ts
+++ src/app/Page/dashboard-page/dashboard-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router, } from '@angular/router';
-import { CategoriesComponent } from "../../Component/categories/categories.component";
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [RouterOutlet, CategoriesComponent],
+  imports: [RouterOutlet],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
