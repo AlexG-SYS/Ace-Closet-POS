43b6ae0ee455a8209098ee77c2bbfed82b921120
AlexG-BGLL-Laptop <support@lotteries.bz>
2025-01-03 14:17:52 -0600

    Update - Invoice Component Started

--- src/app/Component/invoices/invoices.component.ts
+++ src/app/Component/invoices/invoices.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Invoice } from '../../DataModels/invoiceData.model';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-invoices',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatTableModule],
   templateUrl: './invoices.component.html',
-  styleUrl: './invoices.component.scss'
+  styleUrl: './invoices.component.scss',
 })
-export class InvoicesComponent {
+export class InvoicesComponent implements AfterViewInit, OnInit {
+  modalData = false;
+  activeInvoice: Invoice[] = [];
+  dataSource = new MatTableDataSource(this.activeInvoice);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  ngOnInit(): void {
+    this.populateInvoiceTable('active');
+  }
+
+  ngAfterViewInit() {
+    const inputField = document.getElementById(
+      'searchInvoice'
+    ) as HTMLInputElement;
+    const clearButton = document.getElementById('clearButton') as HTMLElement;
+
+    if (clearButton && inputField) {
+      clearButton.addEventListener('click', () => {
+        inputField.value = ''; // Correctly accessing the 'value' property
+        inputField.focus();
+        clearButton.style.display = 'none';
+
+        // Create a mock event with an empty value
+        const mockEvent = { target: { value: '' } } as unknown as Event;
+
+        // Call the applyFilter method with the mock event
+        this.applyFilter(mockEvent);
+      });
+
+      inputField.addEventListener('input', () => {
+        clearButton.style.display = inputField.value ? 'block' : 'none';
+      });
+
+      // Initially hide the clear button
+      clearButton.style.display = 'none';
+    }
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  populateInvoiceTable(arg0: string) {}
 }
